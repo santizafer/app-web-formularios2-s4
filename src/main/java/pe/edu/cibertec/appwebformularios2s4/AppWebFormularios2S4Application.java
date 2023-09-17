7a0bd6cd79aacc0427c7e43e4f45de8e2eb67d26
santizafer <santizafer@gmail.com>
2023-09-17 14:17:24 -0500

    initial commit

---
+++ src/main/java/pe/edu/cibertec/appwebformularios2s4/AppWebFormularios2S4Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebformularios2s4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebFormularios2S4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebFormularios2S4Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/appwebformularios2s4/AppWebFormularios2S4ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebformularios2s4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebFormularios2S4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
