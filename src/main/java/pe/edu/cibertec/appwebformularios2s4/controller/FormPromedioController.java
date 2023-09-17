2cb635c0b105b03006813eed0b52fc2cb9c60694
santizafer <santizafer@gmail.com>
2023-09-17 17:03:33 -0500

    se agregó funcion

--- src/main/java/pe/edu/cibertec/appwebformularios2s4/controller/FormPromedioController.java
+++ src/main/java/pe/edu/cibertec/appwebformularios2s4/controller/FormPromedioController.java
@@ -3,11 +3,33 @@ package pe.edu.cibertec.appwebformularios2s4.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import pe.edu.cibertec.appwebformularios2s4.model.PromedioModel;
 
 @Controller
 public class FormPromedioController {
-    @GetMapping("/formpromedio")
-    public String formpromedio(Model model) {
+    @GetMapping("/promedionotas")
+    public String index(Model model) {
+        model.addAttribute("promediomodel", new PromedioModel());
+        model.addAttribute("verresultado", false);
+        return "formpromedio";
+    }
+
+    @PostMapping("/promedionotas")
+    public String calcularpromedio(
+        @ModelAttribute("promediomodel")
+        PromedioModel promedioModel,
+                Model model){
+        Double promedio = (promedioModel.getCl1()*0.15) +
+                (promedioModel.getCl2()*0.2) +
+                (promedioModel.getCl3()*0.35) +
+                (promedioModel.getProfin()*0.3);
+        String condicion = (promedio>10.5) ? "Aprovado" : "Desaprovado";
+        model.addAttribute("resultado", "El promedio de notas es: " + promedio + "usted se encuentra" + condicion);
+        model.addAttribute("verresultado", true);
+        model.addAttribute("promediomodel", new PromedioModel());
         return "formpromedio";
     }
 }
--- src/main/java/pe/edu/cibertec/appwebformularios2s4/model/PromedioModel.java
+++ src/main/java/pe/edu/cibertec/appwebformularios2s4/model/PromedioModel.java
@@ -1,6 +1,18 @@
 package pe.edu.cibertec.appwebformularios2s4.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PromedioModel {
-
+    private Integer cl1;
+    private Integer cl2;
+    private Integer cl3;
+    private Integer cl4;
+    private Integer profin;
 }
